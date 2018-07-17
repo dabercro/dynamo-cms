b4f8530cd4e8eddf85caf225799c3bd7d95e5d09
Yutaro Iiyama <yiiyama@mit.edu>
2018-07-17 12:00:11 -0400

    redirectors

---
+++ web/html/dynamo/detox.php
@@ -0,0 +1,10 @@
+<?php
+$reqarr = array();
+foreach ($_REQUEST as $key => $value)
+  $reqarr[] = $key . '=' . $value;
+
+header(sprintf("Location: %s://%s/web/detox?%s", $_SERVER["REQUEST_SCHEME"], $_SERVER["HTTP_HOST"], implode('&', $reqarr)));
+
+exit;
+
+?>
