614dd27193a4ab717dd8ac5a083f397091897cca
Yutaro Iiyama <yiiyama@mit.edu>
2018-07-17 12:04:52 -0400

    making it a bit nicer

--- web/html/dynamo/detoxtest.php
+++ web/html/dynamo/detoxtest.php
@@ -3,7 +3,10 @@ $reqarr = array();
 foreach ($_REQUEST as $key => $value)
   $reqarr[] = $key . '=' . $value;
 
-header(sprintf("Location: %s://%s/web/detox/test?%s", $_SERVER["REQUEST_SCHEME"], $_SERVER["HTTP_HOST"], implode('&', $reqarr)));
+if (count($reqarr) != 0) {
+  $request = '?' . implode('&', $reqarr)));
+
+header(sprintf("Location: %s://%s/web/detox/test%s", $_SERVER["REQUEST_SCHEME"], $_SERVER["HTTP_HOST"], $request));
 
 exit;
 
